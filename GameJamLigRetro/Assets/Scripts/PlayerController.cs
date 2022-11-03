fbb6b9bf37b8a9f8598f08b42ec8126783b1a9b0
Shahine28 <shahine93300@gmail.com>
2022-11-03 22:28:21 +0100

    Déplacement Top Down + Tir (Corrigé)

--- GameJamLigRetro/Assets/Scripts/PlayerController.cs
+++ GameJamLigRetro/Assets/Scripts/PlayerController.cs
@@ -7,8 +7,11 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+    public Camera cam;
+
 
     Vector2 movement;
+    Vector2 mousePos;
 
     void Start()
     {
@@ -20,6 +23,8 @@ public class PlayerController : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void FixedUpdate()
@@ -27,5 +32,8 @@ public class PlayerController : MonoBehaviour
 
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
 
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
